a023bb95a6bc1b380b802f5dcc94ef639f249f5a
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-12-25 16:18:27 +0800

    建立TaskController並設定路由

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
